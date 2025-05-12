6ef42cc85647148ad716a99888ed475ecad6ee51
quellus <quellus7@gmail.com>
2025-05-11 19:13:28 -0700

    Moved function definitions to cpp file

--- crow-restapi/src/Data.cpp
+++ crow-restapi/src/Data.cpp
@@ -1,1 +1,43 @@
-#include "driver.h"
+#include "Data.h"
+
+Data& Data::getStaticObject() {
+    static Data data;
+    return data;
+}
+
+Data::Data(Data const& other) {
+    std::unique_lock<std::mutex> lock_other(other.m_mutex);
+    inputs = other.inputs;
+    calls = other.calls;
+}
+
+Data& Data::operator=(Data const& other) {
+    if(&other != this)
+    {
+        std::unique_lock<std::mutex> lock_this(m_mutex, std::defer_lock);
+        std::unique_lock<std::mutex> lock_other(other.m_mutex, std::defer_lock);
+
+        std::lock(lock_this, lock_other);
+
+        inputs = other.inputs;
+        calls = other.calls;
+    }
+
+    return *this;
+}
+
+std::vector<int> Data::getInputs() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    return inputs;
+}
+
+int Data::getCalls() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    return calls;
+}
+
+void Data::input(int newInput) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    inputs.push_back(newInput);
+    calls++;
+}
--- crow-restapi/src/Data.h
+++ crow-restapi/src/Data.h
@@ -1,56 +1,25 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <mutex>
 
 class Data {
 public:
-	static Data& getStaticObject() {
-		static Data data;
-		return data;
-	}
 
 	Data() = default;
 
-	Data(Data const& other) {
-		std::unique_lock<std::mutex> lock_other(other.m_mutex);
-		inputs = other.inputs;
-		calls = other.calls;
-	}
+	static Data& getStaticObject();
 
-	Data& operator=(Data const& other) {
-		if(&other != this)
-        {
-            std::unique_lock<std::mutex> lock_this(m_mutex, std::defer_lock);
-            std::unique_lock<std::mutex> lock_other(other.m_mutex, std::defer_lock);
+	Data(Data const& other);
 
-            std::lock(lock_this, lock_other);
+	Data& operator=(Data const& other);
 
-			inputs = other.inputs;
-			calls = other.calls;
-		}
+	std::vector<int> getInputs();
 
-		return *this;
-	}
-	
-	std::vector<int> getInputs() {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		return inputs;
-	}
+	int getCalls();
 
-	int getCalls() {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		return calls;
-	}
-
-	std::string HelloWorld() {
-		return "Greetings from data!";
-	}
-
-	void input(int newInput) {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		inputs.push_back(newInput);
-		calls++;
-	}
+	void input(int newInput);
 
 private:
 	std::vector<int> inputs = {};
